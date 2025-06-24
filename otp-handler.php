e7cf06e25de94adc9c7fe71816bc36a2b76d6697
MD. Shohel Rana <shohel065931@gmail.com>
2025-06-24 14:26:23 +0600

    gh

--- otp-handler.php
+++ otp-handler.php
@@ -5,14 +5,18 @@ add_action('template_redirect' function() {
         $user_id = intval($_GET['user_id']);
         $otp_input = sanitize_taxt_field($_POST['otp']);
         $stored_otp = get_transient('otp_' . $user_id);
+
+
         if ($stored_otp && $stored_otp == $otp_input) {
             wp_set_auth_cookie($user_id);
             wp_redirect(home_url('/'));
             exit;
         }
-        else
+        else {
+            echo "Invalid OTP!";
+        }
     }
-})
+});
 
 
 
--- templates/otp-form.php
+++ templates/otp-form.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Enter OTP</title>
 </head>
 <body>
     
   <form action="" method="post">
-    <label>Enter OTP</label>
-    <input type="text" name="" />
-    <input type="submit" name="" value="" />
+    <label>Enter OTP:</label>
+    <input type="text" name="otp" />
+    <input type="submit" name="verify_otp" value="Verify" />
   </form>
 
 </body>

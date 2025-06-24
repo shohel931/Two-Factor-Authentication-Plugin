dcf743faa8d6990db8701ba6dfe3f06b08cab1a5
MD. Shohel Rana <shohel065931@gmail.com>
2025-06-24 15:40:19 +0600

    fdg

--- wp-two-factor-mysite.php
+++ wp-two-factor-mysite.php
@@ -54,7 +54,7 @@ add_action('template_redirect', function() {
 
         // Show OTP form
         echo '
-        <!DOCTYPE html>
+         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
@@ -62,13 +62,66 @@ add_action('template_redirect', function() {
             <title>Enter OTP</title>
         </head>
         <body>
+        <style>
+        body{
+            background-color: #F5F5FA;
+        }
+        .otp_form{
+          margin: 0;
+          padding: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 60vh;
+        }
+        .sub_otp_form{
+            background-color: #fff;
+            width: 300px;
+            padding: 30px;
+            height: 180px;
+            align-items: center;
+            justify-content: center;
+            display: flex;
+            text-align: center;
+            border-radius: 10px;
+            box-shadow: 5px 5px 10px #ccc;
+        }
+        .sub_otp_form form label{
+            font-size: 25px;
+            font-weight: 400;
+        }
+        .sub_otp_form form input[type="number"]{
+            width: 200px;
+            padding: 10px;
+            border: 1px solid #ddd;
+            box-shadow: 1px 1px 10px #ccc;
+            border-radius: 5px;
+        }
+        .sub_otp_form form input[type="submit"]{
+            padding: 10px 25px;
+            border: none;
+            background: blue;
+            color: #fff;
+            border-radius: 5px;
+            transition: 0.5s;
+            font-size: 16px;
+            font-weight: bold;
+        }
+        .sub_otp_form form input[type="submit"]:hover{
+            background-color: #000;
+        }
+           
+        
+        </style>
         <div class="otp_form">
-            <form action="" method="post">
-                <label>Enter OTP:</label>
-                <input type="text" name="otp" required />
-                <input type="submit" name="verify_otp" value="Verify" />
+          <div class="sub_otp_form">
+            <form method="post">
+              <label for="otp">Enter OTP:</label><br><br>
+              <input type="number" id="otp" name="otp" placeholder="Enter code" required /><br><br>
+              <input type="submit" name="verify_otp" value="Verify" />
             </form>
-            </div>
+           </div>
+        </div>
         </body>
         </html>';
         exit;
